e86ae35f3e9d5d0fb05554906f6ce5e55e5a4930
Raj Dsouza <rajdsouza@gmail.com>
2015-07-14 11:19:23 +1000

    Update php55-crypto.rb
    Testing the changes with lower version

--- Formula/php55-crypto.rb
+++ Formula/php55-crypto.rb
@@ -3,7 +3,7 @@ require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
 class Php55Crypto < AbstractPhp55Extension
   init
   homepage 'http://pecl.php.net/package/crypto'
-  url 'http://pecl.php.net/get/crypto-0.1.1.tgz'
+  url 'http://pecl.php.net/get/crypto-0.1.0.tgz'
   sha1 '518454c7898d5ba33713a7a8cf2cb2f6acf68824'
   head 'https://github.com/bukka/php-crypto.git'
 
